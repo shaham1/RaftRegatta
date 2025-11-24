271bd955d4c2031c2774713b97c44dd3e305aa18
Shaham Aftab <shaham1>
2025-11-25 00:27:42 +0500

    rolled back to prisma 6.0.0

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,16 +1,16 @@
-import { defineConfig } from '@prisma/config';
+// import { defineConfig } from '@prisma/config';
 
-export default defineConfig({
-  // @ts-expect-error
-  earlyAccess: true,
+// export default defineConfig({
+//   // @ts-expect-error
+//   earlyAccess: true,
   
-  datasources: {
-    db: {
-      provider: 'postgresql',
-      // The Pooled URL (for the app to run fast)
-      url: process.env.POSTGRES_URL, 
-      // The Direct URL (for migrations/push to work without timeout)
-      directUrl: process.env.POSTGRES_URL_NON_POOLING,
-    },
-  },
-});
+//   datasources: {
+//     db: {
+//       provider: 'postgresql',
+//       // The Pooled URL (for the app to run fast)
+//       url: process.env.POSTGRES_URL, 
+//       // The Direct URL (for migrations/push to work without timeout)
+//       directUrl: process.env.POSTGRES_URL_NON_POOLING,
+//     },
+//   },
+// });
