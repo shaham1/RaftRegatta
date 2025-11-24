fd23a21108b9a449187a625233eb8a968c89aba9
Shaham Aftab <shaham1>
2025-11-24 23:56:34 +0500

    bidding api endpoint

--- src/app/api/bid/route.ts
+++ src/app/api/bid/route.ts
@@ -1,13 +1,58 @@
 import { NextRequest, NextResponse } from 'next/server';
+import { prisma } from '@/lib/prisma';
+
 
 export async function POST(request: NextRequest) {
-  console.log("PING RECEIVED from Colab!");
+  try {
+    console.log("Bid Request Received");
 
-  const body = await request.json();
-  console.log("Payload:", body);
+    const apiKey = request.headers.get('x-auction-key');
+    const body = await request.json();
+    const bidAmount = parseFloat(body.amount);
 
-  return NextResponse.json({ 
-    message: "Connection successful!", 
-    received_data: body 
-  }, { status: 200 });
+    if (!apiKey) return NextResponse.json({ error: 'Missing API Key' }, { status: 401 });
+    if (isNaN(bidAmount)) return NextResponse.json({ error: 'Invalid Amount' }, { status: 400 });
+
+    const team = await prisma.teams.findUnique({
+      where: { apiKey: apiKey },
+    });
+
+    if (!team) {
+      console.log("Invalid Key:", apiKey);
+      return NextResponse.json({ error: 'Invalid API Key' }, { status: 403 });
+    }
+
+    const currentRound = await prisma.round.findFirst({
+      where: { status: 'OPEN' },
+    });
+
+    if (!currentRound) {
+      return NextResponse.json({ error: 'No round is currently open' }, { status: 400 });
+    }
+
+    const newBid = await prisma.bid.create({
+      data: {
+          roundId: currentRound.id,
+          teamNo: team.teamNo,
+          teamName: team.teamName,
+          item: "Unkown",
+          amount: bidAmount,
+      },
+    });
+
+    console.log(`Bid Accepted: Team ${team.teamNo} bid $${bidAmount}`);
+    
+    return NextResponse.json({ 
+      success: true, 
+      bid_id: newBid.id 
+    }, { status: 200 });
+
+  } catch (error: any) {
+
+    if (error.code === 'P2002') {
+      return NextResponse.json({ error: 'You have already placed a bid in this round.' }, { status: 400 });
+    }
+    console.error("Server Error:", error);
+    return NextResponse.json({ error: 'Server Error' }, { status: 500 });
+  }
 }
--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,18 +1,11 @@
-import * as Prisma from '@prisma/client';
-import { PrismaPg } from '@prisma/adapter-pg';
+import { PrismaClient } from '@prisma/client';
 
-const { PrismaClient } = Prisma;
+const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
-const globalForPrisma = global as unknown as {
-  prisma: typeof PrismaClient;
-};
-
-const adapter = new PrismaPg({
-  connectionString: process.env.DATABASE_URL,
-});
-
-const prisma = globalForPrisma.prisma || new PrismaClient({ adapter });
+export const prisma =
+  globalForPrisma.prisma ||
+  new PrismaClient({
+    log: ['query'], 
+  });
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
-
-export default prisma;
